41d3bc2627657af94b3051ba451c95413c01318a
Shailesh8010 <121032736+Shailesh8010@users.noreply.github.com>
2024-02-23 14:04:04 +0530

    Add files via upload

---
+++ Shailesh Gaikwad 15-02-2024 (2)/Shailesh Gaikwad 14-02-2024/Script.js
@@ -0,0 +1,105 @@
+// // Primitive Data Types
+// // Number
+// let age = 25;
+// console.log("Age:", age, typeof age);
+
+// // String
+// let name = "Shailesh";
+// console.log("Name:", name,typeof name);
+
+// // Boolean
+// let isStudent = true;
+// console.log("Is Student:", isStudent,typeof isStudent);
+
+// // Undefined
+// let grade;
+// console.log("Grade:", grade,typeof grade);
+
+// // Null
+// let car = null;
+// console.log("Car:", car, typeof car);
+
+
+
+
+// // Non-primitive Data Type
+
+// // Object
+// let person = {
+//   name: "Shailesh",
+//   age: 30,
+//   isStudent: false
+// };
+// console.log("Person:", person,typeof person);
+
+// // Array
+// let Arr=["Shailesh",30,false,undefined,null]
+// console.log(Arr)
+
+
+
+
+// // difference betn let var and const
+// const b=55
+
+// {
+//     var a =23;
+// }
+
+// console.log(b)
+
+
+
+// //Types Of Functions
+
+// // Named Functions
+// function abc(name) {
+//     return "Hello, " + name + "!";
+//   }
+//   console.log(abc("Shailesh")); 
+
+
+// // Anonymous Function
+//   let xyz = function(name) {
+//     return "Hello, " + name + "!";
+//   };
+//   console.log(xyz("Shailesh"));
+
+
+// // Arrow Function
+//   let pqr = (name) => {
+//     return "Hello, " + name + "!";
+//   };
+//    console.log(pqr("Shailesh"));
+
+
+
+
+
+
+
+
+
+
+
+// Some Examples of Event Handelers
+
+//  const button = document.getElementById('myButton');
+//  button.addEventListener('click', function() {
+//    alert('Button clicked!');
+//  });
+
+ 
+//  const div = document.getElementById('myDiv');
+//  div.addEventListener('mouseover', function() {
+//    div.style.backgroundColor = 'green';
+//  });
+//  div.addEventListener('mouseout', function() {
+//     div.style.backgroundColor = 'blue';
+//   });
+
+ 
+//  const input = document.getElementById('myInput');
+//  input.addEventListener('keydown', function(event) {
+//    console.log('Key pressed:', event.key);
+//  });
